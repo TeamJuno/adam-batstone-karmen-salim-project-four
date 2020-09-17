af11a796a3c162156582dd1577731d580d0171d4
skarmen <karmennsalim@gmail.com>
2020-09-17 17:05:41 -0400

    Configured the results from the API call

--- scripts/app.js
+++ scripts/app.js
@@ -1,8 +1,8 @@
 // Pseudo Code
-//   - User enters search term
-//     - save user input into variable
-//       - use the variable as the query value
-//         - return all results that match the user input
+//   - User enters search term- DONE
+//     - save user input into variable - DONE
+//       - use the variable as the query value - DONE
+//         - return all results that match the user input - DONE
 //           - (displayItems()) use forEach loop on the results object to create an < li > for each and append to the < ul > on the DOM.Each < li > will be linked by the 'idDrink' from the results object.
 // - Within each < li > a title and picture will display for the drink.The user can click on the title to access the recipe, which will result in another API call using the 'idDrink'.This will be displayed in a < div > next to the drink image.
 // - Append / Prepend the recipe details to the ul
@@ -22,20 +22,62 @@
 const cocktailApp = {}
 
 
+
 // API Call
-function getRecipes(ingredient) {
+cocktailApp.getRecipes = function(ingredient) {
   return $.ajax({
-    url: `https://www.thecocktaildb.com/api/json/v1/1/search.php?i=${ingredient}`,
+    url: `https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=${ingredient}`,
     method: 'GET',
     dataType: 'JSON',
+  })
+  .then((res)=>{
+    // define empty arr to push a limited number of recipes to
+    const recipesToDisplay = []
+
+    // pick random 20 recipes to push back to recipesToDisplay
+    for(let i = 0; i<= 20; i++) {
+
+      // Get random recipes from the results object
+      const randomRecipes = Math.floor(Math.random() * res.drinks.length)
+      console.log('randomRecipes:', randomRecipes)
+
+      recipesToDisplay.push(res.drinks[randomRecipes])
+      console.log('recipesToDisplay',recipesToDisplay)
+    }
+  })
+  .fail((err) => {
+    console.log(err)
+  })
+}
+
+
+/* Configure Submit Behaviour
+  - get the user input
+  - call the getRecipes()
+*/
+cocktailApp.onSubmit = function () {
+  $('form').on('submit', (e) => {
+    e.preventDefault()
+    console.log('Form submit is working..')
+    console.log('event target:', e.target)
+
+    // Store user input in a variable
+    const $userInput = $('input').val().trim()
+    console.log('$userInput:', $userInput)
+
+    // Call the API to return recipes to the user
+    cocktailApp.getRecipes($userInput)
+
   })
 }
 
 
 // Code to kick off the app
 cocktailApp.init = function () {
-
+  cocktailApp.getRecipes()
+  cocktailApp.onSubmit()
 }
+
 
 // Everything that needs to run on page load
 $(function(){
